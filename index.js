6e592eb27486925a4942c4b934f5476d0e3f8acc
RobinStut <robin.stut@hva.nl>
2019-05-25 17:21:24 +0200

    adding database

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
-var rp = require('request-promise');
-var cheerio = require('cheerio');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+require('dotenv').config()
+require("dotenv-json")();
+const apiKey = process.env.apiKey;
+const authDomain = process.env.authDomain;
+const databaseURL = process.env.databaseURL;
+const projectId = process.env.projectId;
+const storageBucket = process.env.storageBucket;
+const messagingSenderId = process.env.messagingSenderId;
+const appId = process.env.appId;
+const firebase = require('firebase');
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -15,41 +27,59 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => res.render("pages/index"));
 app.listen(process.env.PORT || 3000);
 
-// rp('https://festivalfans.nl/agenda/')
-//   .then(function (htmlString) {
-//     console.log(htmlString);
-//     // Process html...
-//   })
-//   .catch(function (err) {
-//     // Crawling failed...
-//   });
+const admin = require('firebase-admin');
 
+const serviceAccount = require('./static/backend-robinstut-firebase-adminsdk-e7366-9cc3556bac.json')
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://backend-robinstut.firebaseio.com"
+});
 
-var options = {
-  uri: 'https://festivalfans.nl/agenda/',
-  transform: function (body) {
-    return cheerio.load(body);
-  }
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
 };
 
-rp(options)
-  .then(function ($) {
-    let counter = 0
-    let scrapedFestivalData = []
-    $('div.ev2page').children("script").each(function (i, element) {
-      var a = $(this).html()
+firebase.initializeApp(firebaseConfig);
 
-      if (counter < 50) {
-        scrapedFestivalData.push(a)
-      }
-      counter + 1;
-    });
-    console.log(scrapedFestivalData);
-
-
+return firebase.database().ref('/festivalLijst/').once('value').then(function (snapshot) {
+    const timestamp = snapshot.val()[0]
+    return timestamp
   })
-  .catch(function (err) {
-    console.log(err);
-    // Crawling failed or Cheerio choked...
-  });
+  .then(timestampInDb => {
+    if (Date.now() > (timestampInDb + 86400000)) {
+      // get new data
+      let scrapedFestivalData = [Date.now(), ]
+      const options = {
+        uri: 'https://festivalfans.nl/agenda/',
+        transform: function (body) {
+          return cheerio.load(body);
+        }
+      };
+
+      rp(options)
+        .then(function ($) {
+          $('div.ev2page').children("script").each(function (i, element) {
+            const a = JSON.parse($(this).html())
+            scrapedFestivalData.push(a)
+          });
+        })
+        .then(write => {
+          firebase.database().ref('festivalLijst/').set(scrapedFestivalData);
+        })
+
+        .catch(function (err) {
+          console.log(err);
+        });
+
+    } else {
+      //use old data
+    }
+  })
