dc99cc6e0d15e68b9b52d442e18cc1f53927307f
RobinStut <robin.stut@hva.nl>
2019-05-25 20:55:52 +0200

    datamanagement change

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const messagingSenderId = process.env.messagingSenderId;
 const appId = process.env.appId;
 const firebase = require('firebase');
 
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -24,7 +22,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, "./static")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.get("/", (req, res) => res.render("pages/index"));
+
 app.listen(process.env.PORT || 3000);
 
 const admin = require('firebase-admin');
@@ -49,14 +47,34 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+
+
+app.get("/", async (req, res) => {
+
+  function retreiveFestivalData() {
+    return firebase.database().ref('/festivalLijst/').once('value').then(function (snapshot) {
+      const data = snapshot.val()
+      return data
+    })
+  }
+
+  const retreivedData = await retreiveFestivalData()
+
+  // console.log(retreivedData);
+  res.render("pages/index", {
+    data: retreivedData
+  })
+  // res.json(retreivedData)
+});
+
 return firebase.database().ref('/festivalLijst/').once('value').then(function (snapshot) {
     const timestamp = snapshot.val()[0]
     return timestamp
   })
-  .then(timestampInDb => {
+  .then(data => {
+    let scrapedFestivalData = [Date.now(), ]
     if (Date.now() > (timestampInDb + 86400000)) {
-      // get new data
-      let scrapedFestivalData = [Date.now(), ]
+
       const options = {
         uri: 'https://festivalfans.nl/agenda/',
         transform: function (body) {
@@ -67,12 +85,50 @@ return firebase.database().ref('/festivalLijst/').once('value').then(function (s
       rp(options)
         .then(function ($) {
           $('div.ev2page').children("script").each(function (i, element) {
-            const a = JSON.parse($(this).html())
-            scrapedFestivalData.push(a)
+            const scriptTag = JSON.parse($(this).html())
+            console.log('object');
+            console.log(scriptTag);
+
+            const rx = /(\d{4})-(\d{2})-(\d{2})T(\d{2}:\d{2}).*/g
+
+            let startDateObj
+            const startDate = scriptTag.startDate.replace(rx, (...x) => {
+              startDateObj = {
+                year: x[1],
+                month: x[2],
+                day: x[3],
+                hour: x[4]
+              }
+            })
+
+            let endDateObj
+            const endDate = scriptTag.endDate.replace(rx, (...x) => {
+              endDateObj = {
+                year: x[1],
+                month: x[2],
+                day: x[3],
+                hour: x[4]
+              }
+            })
+
+            const newdata = {
+              name: scriptTag.name,
+              plaats: scriptTag.location.address.addressLocality,
+              adres: scriptTag.location.name,
+              startDate: startDateObj,
+              endDate: endDateObj,
+              image: scriptTag.image
+
+            }
+            // console.log('newdata' + newdata);
+
+            scrapedFestivalData.push(newdata);
+            console.log('objectend');
           });
         })
         .then(write => {
           firebase.database().ref('festivalLijst/').set(scrapedFestivalData);
+
         })
 
         .catch(function (err) {
@@ -81,5 +137,7 @@ return firebase.database().ref('/festivalLijst/').once('value').then(function (s
 
     } else {
       //use old data
+      scrapedFestivalData.push(data)
+      console.log('use old data');
     }
   })
